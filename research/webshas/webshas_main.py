cc9a9f89516e4dc9ca037011b675b12eac339547
Joshua <joshuaegoldmeier@gmail.com>
2019-05-17 17:26:09 +0300

    added junk code

---
+++ research/webshas/webshas_main.py
@@ -0,0 +1,26 @@
+# # -*- coding: utf-8 -*-
+# import urllib
+# import urllib2
+# from urllib2 import URLError, HTTPError
+# import json
+# import pdb
+# import os
+# import sys
+# from bs4 import BeautifulSoup
+# import re
+#
+# p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.insert(0, p)
+# sys.path.insert(0, '../Match/')
+# from match import Match
+#
+# os.environ['DJANGO_SETTINGS_MODULE'] = "sefaria.settings"
+# from local_settings import *
+# from functions import *
+#
+# sys.path.insert(0, SEFARIA_PROJECT_PATH)
+# from sefaria.model import *
+# from sefaria.model.schema import AddressTalmud
+
+
+print("worked") 
