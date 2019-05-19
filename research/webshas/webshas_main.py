7bdc533ffacc863895037b7acb1ca122a195515b
Joshua <joshuaegoldmeier@gmail.com>
2019-05-19 21:02:29 +0300

    got it to rungit status

--- research/webshas/webshas_main.py
+++ research/webshas/webshas_main.py
@@ -1,26 +1,17 @@
-# # -*- coding: utf-8 -*-
-# import urllib
-# import urllib2
-# from urllib2 import URLError, HTTPError
-# import json
-# import pdb
-# import os
+# -*- coding: utf-8 -*-
 # import sys
-# from bs4 import BeautifulSoup
-# import re
-#
-# p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# sys.path.insert(0, p)
-# sys.path.insert(0, '../Match/')
-# from match import Match
-#
-# os.environ['DJANGO_SETTINGS_MODULE'] = "sefaria.settings"
-# from local_settings import *
-# from functions import *
-#
-# sys.path.insert(0, SEFARIA_PROJECT_PATH)
-# from sefaria.model import *
-# from sefaria.model.schema import AddressTalmud
+import sys
+import os
+import django
+from sources.local_settings import *
 
 
-print("worked") 
+sys.path.insert(0, SEFARIA_PROJECT_PATH)
+os.environ['DJANGO_SETTINGS_MODULE'] = "sefaria.settings"
+
+
+django.setup()
+
+from sefaria.model import *
+
+print("worked")
